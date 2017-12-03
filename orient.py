5d0701dc366fbbf65c0eee76686c7a3f807337a9
Unknown <vsriniv@iu.edu>
2017-12-02 19:58:37 -0500

    updated indents

--- orient.py
+++ orient.py
@@ -6,23 +6,27 @@ import operator
 from Queue import PriorityQueue
 
 def knnTrain(trainFile, modelFile):
+    print "Learning Model.."
     trainData = open(trainFile, "r")
-    modelAppend = open(modelFile, "w+")
+    modelAppend = open(modelFile, "w")
     for line in trainData:
         row = line[:-1].split(' ', 2)
         modelAppend.write("%s|%s\n" % (row[1], row[2]))
     trainData.close()
     modelAppend.close()
+    print "Learning complete.."
 
 def knnTest(testFile, modelFile):
     distQueue = PriorityQueue()
     testData = open(testFile, "r")
     model = open(modelFile, "r")
+    print "Begin test processing.."
     n = 0
     accuracy = 0
     for line in testData:
         knn = {'0': 0, '90': 0, '180': 0, '270': 0}
         n += 1
+        print "--------------------------------------------------------------------------------------------------------------" + str(n)
         testList = line.split(' ')
         testOrient = testList[1]
         testVector = testList[2:]
@@ -39,52 +43,27 @@ def knnTest(testFile, modelFile):
         for i in range(0, k, 1):
             knnOrient = distQueue.get()
             knn[knnOrient[1]] += 1
-        predictOrient = max(knn, key=knn.get)
-        accuracy += (1 if int(predictOrient) == int (testOrient) else 0)
+            predictOrient = max(knn, key=knn.get)
+        accuracy += (1 if int(predictOrient) == int(testOrient) else 0)
         if predictOrient != testOrient:
-            #print predictOrient
-            #print testOrient
-            #print knn
-            #print "--------------------------------------------------------------------------------------------------------------" + str(n)
-    #print "Accuracy: " + str(100.0 * accuracy / n)
-#model file only weights
-
-
-
-def adaboostTrain(trainFile, modelFile):
-    vectorlist = []
-    orientlist = [] #membership function
-    wt = 1 #start with 1
-    wtlist = [] #weight list for corresponding vector
-    trainData = open(trainFile, "r")
-    modelAppend = open(modelFile, "w+")
-    for line in trainData:
-        row = line[:-1].split(' ', 2)
-        vectorlist.append(row[2:])
-        orientlist.append(row[1])
-        print vectorlist
-    for sth in range(0,len(vectorlist)-1):
-        wtlist.append(wt)
-    modelAppend.write(wtlist)
-    modelAppend.close()
-
-
-
-#def adaboostTest():
+            print predictOrient
+            print testOrient
+            print knn
+            print "Accuracy: " + str(100.0 * accuracy / n)
 
 
 switch = sys.argv[1]
 switchFile = sys.argv[2]
-model = sys.argv[3]
-modelFile = sys.argv[4]
+modelFile = sys.argv[3]
+model = sys.argv[4]
 
 if model.lower() == "nearest":
     if switch.lower() == "train":
         knnTrain(switchFile, modelFile)
     if switch.lower() == "test":
         knnTest(switchFile, modelFile)
-elif model.lower() == "adaboost":
-    if switch.lower() == "train":
-        adaboostTrain(switchFile, modelFile)
-    #if switch.lower() == "test":
-        #adaboostTest(switchFile, modelFile)
+    elif model.lower() == "adaboost":
+        if switch.lower() == "train":
+            adaboostTrain(switchFile, modelFile)
+#if switch.lower() == "test":
+#adaboostTest(switchFile, modelFile)

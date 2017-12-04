a49b291983120ce4e9ddaf226931b65ad87a4d68
Ayesha Bhimdiwala <aybhimdi@iu.edu>
2017-12-04 01:20:16 -0500

    Adaboost.py update, "+1" changes to orient.py and knn.py

--- orient.py
+++ orient.py
@@ -21,29 +21,29 @@ if model.lower() == "nearest":
         accuracy = 0
         numLinesTrain = sum(1 for line in open(modelFile))
         numLinesTest = sum(1 for line in open(switchFile))
-        trainVector = np.zeros((numLinesTrain + 1, 192), dtype=np.int_)
-        trainOrient = np.zeros((numLinesTrain + 1, 1), dtype=np.int_)
-        testVector = np.zeros((numLinesTest + 1, 192), dtype=np.int_)
-        testOrient = np.zeros((numLinesTest + 1, 1), dtype=np.int_)
+        trainVector = np.zeros((numLinesTrain, 192), dtype=np.int_)
+        trainOrient = np.zeros((numLinesTrain, 1), dtype=np.int_)
+        testVector = np.zeros((numLinesTest, 192), dtype=np.int_)
+        testOrient = np.zeros((numLinesTest, 1), dtype=np.int_)
         lineNumber = 0
         model = open(modelFile, "r")
         for row in model:
-            lineNumber += 1
             rowList = row.split('|')
             intmOrient = rowList[0]
             intmVector = rowList[1].split(' ')
             vector = [int(i) for i in intmVector]
             trainOrient[lineNumber] = intmOrient
             trainVector[lineNumber] = np.array(vector)
+            lineNumber += 1
         model.close()
         lineNumber = 0
         testData = open(switchFile, "r")
         for line in testData:
-            lineNumber += 1
             testList = line.split(' ')
             testList = [int(i) for i in testList[1:]]
             testOrient[lineNumber] = testList[0]
             testVector[lineNumber] = np.array(testList[1:])
+            lineNumber += 1
         testData.close()
         for row in range(0, len(testOrient), 1):
             #print "-------------LINE " + str(row) + "-------------"

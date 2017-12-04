d044827deddeae46c104a0eaa7731eacaca4b954
Unknown <vsriniv@iu.edu>
2017-12-03 20:32:21 -0500

    updated for adaboost requirement

--- orient.py
+++ orient.py
@@ -18,7 +18,39 @@ if model.lower() == "nearest":
     if switch.lower() == "train":
         knnTrain(switchFile, modelFile)
     if switch.lower() == "test":
-        knnTest(switchFile, modelFile)
+        accuracy = 0
+        numLinesTrain = sum(1 for line in open(modelFile))
+        numLinesTest = sum(1 for line in open(switchFile))
+        trainVector = np.zeros((numLinesTrain + 1, 192), dtype=np.int_)
+        trainOrient = np.zeros((numLinesTrain + 1, 1), dtype=np.int_)
+        testVector = np.zeros((numLinesTest + 1, 192), dtype=np.int_)
+        testOrient = np.zeros((numLinesTest + 1, 1), dtype=np.int_)
+        lineNumber = 0
+        model = open(modelFile, "r")
+        for row in model:
+            lineNumber += 1
+            rowList = row.split('|')
+            intmOrient = rowList[0]
+            intmVector = rowList[1].split(' ')
+            vector = [int(i) for i in intmVector]
+            trainOrient[lineNumber] = intmOrient
+            trainVector[lineNumber] = np.array(vector)
+        model.close()
+        lineNumber = 0
+        testData = open(switchFile, "r")
+        for line in testData:
+            lineNumber += 1
+            testList = line.split(' ')
+            testList = [int(i) for i in testList[1:]]
+            testOrient[lineNumber] = testList[0]
+            testVector[lineNumber] = np.array(testList[1:])
+        testData.close()
+        for row in range(0, len(testOrient), 1):
+            #print "-------------LINE " + str(row) + "-------------"
+            predictOrient = knnTest(testVector[row], trainOrient, trainVector)
+            accuracy += (1 if predictOrient == int(testOrient[row]) else 0)
+        print "Accuracy: " + str(100.0 * accuracy / row)
+
 elif model.lower() == "adaboost":
     if switch.lower() == "train":
         adaboostTrain(switchFile, modelFile)

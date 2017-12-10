7374861b812ec9087cd49370584f3eacbb82d566
Unknown <vsriniv@iu.edu>
2017-12-10 15:32:22 -0500

    Commit adaBoost and KNN.
    Improved accuracy.
    Decision stumps.

--- adaboost.py
+++ adaboost.py
@@ -5,88 +5,115 @@ import math
 import operator
 from Queue import PriorityQueue
 import numpy as np
-from knn import *
-import numpy as np
+import random
+
+def buildDS(trainFile):
+    global dsVector
+    global dsOrient
+    global numLinesTrain
+    train = open(trainFile, "r")
+    dsVector = np.zeros((numLinesTrain, 192), dtype=np.int_)
+    dsOrient = np.zeros((numLinesTrain, 1), dtype=np.int_)
+    for lineNumber, row in enumerate(train):
+        rowList = row.split(' ', 2)
+        dsOrient[lineNumber] = int(rowList[1])
+        dsVector[lineNumber] = np.array([int(i) for i in rowList[2].split(' ')])
+    train.close()
+
+def buildTrain(orientation, trainWtList):
+    global trainVector
+    global trainOrient
+    global featUdrCns
+    global wtList
+    global rdmRowIdx
+    global numLinesTrain
+    global dsVector
+    global dsOrient
+    rdmRowIdx = np.random.choice([i for i in range(numLinesTrain)], (numLinesTrain*60/100), replace=False, p=trainWtList.ravel())
+    rdmRowIdx.sort()
+    trainVector = np.zeros((len(rdmRowIdx), len(featUdrCns)), dtype=np.int_)
+    trainOrient = np.zeros((len(rdmRowIdx), 1), dtype=np.int_)
+    wtList = np.zeros((len(rdmRowIdx), 1), dtype=np.float_)
+    ridx = 0
+    for row in range(numLinesTrain):
+        if row in rdmRowIdx:
+            trainOrient[ridx] = 1 if dsOrient[row] == orientation else 0
+            trainVector[ridx] = [dsVector[row][col] for col in featUdrCns]
+            wtList[ridx] = trainWtList[row]
+            ridx += 1
+
+def decisionStump(vectorList):
+    return 1 if vectorList[0] > vectorList[1] else 0
 
 def adaboostTrain(trainFile, modelFile):
-    modelappend = open(modelFile, "w")
-    knnTrain(trainFile, modelFile+".ada")
-    model = open(modelFile+".ada", "r")
-    numLinesTrain = sum(1 for line in open(modelFile+".ada"))
-    alphalist = []
-    wtlist = np.full((numLinesTrain, 1),(1.0 / numLinesTrain), dtype=np.float_)
-    kValue = 7
-    lineNumber = 0
-    trainVector = np.zeros((numLinesTrain, 192), dtype=np.int_)
-    trainOrient = np.zeros((numLinesTrain, 1), dtype=np.int_)
-    testVector = np.zeros((numLinesTrain, 192), dtype=np.int_)
-    testOrient = np.zeros((numLinesTrain, 1), dtype=np.int_)
-    for row in model:
-        rowList = row.split('|')
-        intmOrient = rowList[0]
-        intmVector = rowList[1].split(' ')
-        vector = [int(i) for i in intmVector]
-        trainOrient[lineNumber] = intmOrient
-        trainVector[lineNumber] = np.array(vector)
-        lineNumber += 1
-    model.close()
-    trainData = open(trainFile, "r")
-    lineNumber = 0
-    for row in trainData:
-        rowList = row[:-1].split(' ', 2)
-        intmOrient = rowList[1]
-        intmVector = rowList[2].split(' ')
-        vector = [int(i) for i in intmVector]
-        testOrient[lineNumber] = intmOrient
-        testVector[lineNumber] = np.array(vector)
-        lineNumber += 1
-    trainData.close()
-    for element in [0,90,180,270]:
-        wc = 0
-        wnc = 0
-        correctnesslist = np.zeros((numLinesTrain, 1), dtype=np.int_)
-        lineNumber = 0
-        print "*******************************" + str(element) + "*******************************"
-        for item in range(0,len(testOrient),1):
-            print "--------------Line" +str(lineNumber)+ "-----------------"
-            vectorlist = testVector[item]
-            orientval = int(testOrient[item])
-            pred = knnTest(vectorlist, trainOrient, trainVector, kValue)
-            #print pred
-            #print orientval
-            if pred == element and orientval == element:
-                correctnesslist[lineNumber] = True
-                wc += 1
-            elif pred != element and orientval != element:
-                correctnesslist[lineNumber] = True
-                wc += 1
-            else:
-                correctnesslist[lineNumber] = False
-                wnc += 1
-            #print correctnesslist[lineNumber]
-            lineNumber+=1
-        alpha = (wc+1)/float(wnc+1)
-        alphalist.append(alpha)
-        for el in range(len(wtlist)):
-            if correctnesslist[el] == True:
-                wtlist[el] = math.exp(-1*alpha)
-            else:
-                wtlist[el] = math.exp(alpha)
-        z_t = np.sum(wtlist)
-        wtlist /= z_t
-        e_t = wc/float(wc+wnc)*100
-        print e_t
-    modelappend.write("%s\n" % (' '.join(str(item) for item in alphalist)))
-    modelappend.close()
-    modelappend = open(modelFile, "a")
-    for item in range(0, len(testOrient), 1):
-        modelappend.write("%s %s\n" % (' '.join(str(item) for item in testOrient[item]), ' '.join(str(item) for item in testVector[item])))
-    modelappend.close()
+    modelAppend = open(modelFile, "w+")
+    global numLinesTrain
+    global rdmRowIdx
+    global featUdrCns
+    global wtList
+    numLinesTrain = sum(1 for line in open(trainFile))
+    trainWtList = np.ones((numLinesTrain, 1), dtype=np.float_) * (1.0 / numLinesTrain)
+    buildDS(trainFile)
+    for element in [0, 90, 180, 270]:
+        for classifier in range(400):
+            pred = []
+            featUdrCns = random.sample(xrange(192), 2)
+            featUdrCns.sort()
+            buildTrain(element, trainWtList)
+            for item in range(len(trainOrient)):
+                vectorList = trainVector[item]
+                orientVal = int(trainOrient[item])
+                pred.append(1 if orientVal == (decisionStump(vectorList)) else 0)
+            alpha = (0.5) * math.log((pred.count(1) + 1) / float(pred.count(0) + 1))
+            modelAppend.write("%s %s %s\n" % (str(element), ' '.join(str(i) for i in featUdrCns), str(alpha)))
+            for el in range(len(wtList)):
+                wtList[el] = math.exp(-1 * alpha) if pred[el] == 1 else math.exp(alpha)
+            wtList /= np.sum(wtList)
+            error = (pred.count(1) / float(pred.count(1) + pred.count(0))) * 100
+            #print error
+            ridx = 0
+            for row in range(numLinesTrain):
+                if row in rdmRowIdx:
+                    trainWtList[row] = wtList[ridx]
+                    ridx += 1
+            trainWtList /= np.sum(trainWtList)
+        print "Classifier completed " + str(element)
+    modelAppend.close()
 
-        # for sth in range(0,len(vectorlist)-1):
-    #     wtlist.append(wt)
-    # modelAppend.write(wtlist)
-    # modelAppend.close()
-
-def adaboostTest(trainFile, modelFile):
-    pass
+def adaboostTest(testFile, modelFile):
+    test = open(testFile, "r")
+    model = open(modelFile, "r")
+    accuracy = 0
+    numLinesTest = sum(1 for line in open(testFile))
+    numLinesModel = sum(1 for line in open(modelFile))
+    dsFleTst = np.empty(numLinesTest, dtype='S256')
+    dsOntTst = np.zeros((numLinesTest, 1), dtype=np.int_)
+    dsVtrTst = np.zeros((numLinesTest, 192), dtype=np.int_)
+    dsOntMdl = np.zeros((numLinesModel, 1), dtype=np.int_)
+    dsFtrMdl = np.zeros((numLinesModel, 2), dtype=np.int_)
+    dsAphMdl = np.zeros((numLinesModel, 1), dtype=np.float_)
+    for lineNumber, row in enumerate(test):
+        rowList = row.split(' ', 2)
+        dsFleTst[lineNumber] = rowList[0]
+        dsOntTst[lineNumber] = int(rowList[1])
+        dsVtrTst[lineNumber] = np.array([int(i) for i in rowList[2].split(' ')])
+    test.close()
+    for lineNumber, row in enumerate(model):
+        rowList = row.split(' ')
+        dsOntMdl[lineNumber] = int(rowList[0])
+        dsFtrMdl[lineNumber] = [int(rowList[1]), int(rowList[2])]
+        dsAphMdl[lineNumber] = float(rowList[3])
+    test.close()
+    for lineNumberT, rowT in enumerate(dsOntTst):
+        pred = {0: 0, 90: 0, 180: 0, 270: 0}
+        for lineNumberM, rowM in enumerate(dsOntMdl):
+            orientVal = decisionStump([dsVtrTst[lineNumberT][col] for col in dsFtrMdl[lineNumberM]])
+            if orientVal == 1:
+                pred[int(rowM)] += float(dsAphMdl[lineNumberM])
+            else:
+                pred[int(rowM)] -= float(dsAphMdl[lineNumberM])
+        predOnt = max(pred.iteritems(), key=operator.itemgetter(1))[0]
+        print dsFleTst[lineNumberT] + " " + str(predOnt)
+        if predOnt == dsOntTst[lineNumberT]:
+            accuracy += 1
+    print "Adaboost Accuracy: " + str((accuracy/float(numLinesTest))*100)
--- orient.py
+++ orient.py
@@ -9,6 +9,29 @@ import numpy as np
 from adaboost import *
 from knn import *
 
+def knnTestPreprocessor():
+    lineNumber = 0
+    model = open(modelFile, "r")
+    for row in model:
+        rowList = row.split('|')
+        intmOrient = rowList[0]
+        intmVector = rowList[1].split(' ')
+        vector = [int(i) for i in intmVector]
+        trainOrient[lineNumber] = intmOrient
+        trainVector[lineNumber] = np.array(vector)
+        lineNumber += 1
+    model.close()
+    lineNumber = 0
+    testData = open(switchFile, "r")
+    for line in testData:
+        testList = line.split(' ')
+        testFile[lineNumber] = testList[0]
+        testList = [int(i) for i in testList[1:]]
+        testOrient[lineNumber] = testList[0]
+        testVector[lineNumber] = np.array(testList[1:])
+        lineNumber += 1
+    testData.close()
+
 switch = sys.argv[1]
 switchFile = sys.argv[2]
 modelFile = sys.argv[3]
@@ -19,42 +42,24 @@ if model.lower() == "nearest":
         knnTrain(switchFile, modelFile)
     if switch.lower() == "test":
         accuracy = 0
-        kValue = 47
         numLinesTrain = sum(1 for line in open(modelFile))
         numLinesTest = sum(1 for line in open(switchFile))
         trainVector = np.zeros((numLinesTrain, 192), dtype=np.int_)
         trainOrient = np.zeros((numLinesTrain, 1), dtype=np.int_)
         testVector = np.zeros((numLinesTest, 192), dtype=np.int_)
         testOrient = np.zeros((numLinesTest, 1), dtype=np.int_)
-        lineNumber = 0
-        model = open(modelFile, "r")
-        for row in model:
-            rowList = row.split('|')
-            intmOrient = rowList[0]
-            intmVector = rowList[1].split(' ')
-            vector = [int(i) for i in intmVector]
-            trainOrient[lineNumber] = intmOrient
-            trainVector[lineNumber] = np.array(vector)
-            lineNumber += 1
-        model.close()
-        lineNumber = 0
-        testData = open(switchFile, "r")
-        for line in testData:
-            testList = line.split(' ')
-            testList = [int(i) for i in testList[1:]]
-            testOrient[lineNumber] = testList[0]
-            testVector[lineNumber] = np.array(testList[1:])
-            lineNumber += 1
-        testData.close()
+        testFile = np.empty(numLinesTest, dtype='S256')
+        knnTestPreprocessor()
         for row in range(0, len(testOrient), 1):
-            #print "-------------LINE " + str(row) + "-------------"
-            predictOrient = knnTest(testVector[row], trainOrient, trainVector, kValue)
+            knn = {0: 0, 90: 0, 180: 0, 270: 0}
+            knnDist = {0: 0, 90: 0, 180: 0, 270: 0}
+            # print "-------------LINE " + str(row) + "-------------"
+            predictOrient = knnTest(testVector[row], trainOrient, trainVector, 45, knn, knnDist)
+            print str(testFile[row]) + " " + str(predictOrient)
             accuracy += (1 if predictOrient == int(testOrient[row]) else 0)
         print "K-Nearest Neighbours Accuracy: " + str(100.0 * accuracy / row)
-
 elif model.lower() == "adaboost":
     if switch.lower() == "train":
         adaboostTrain(switchFile, modelFile)
     if switch.lower() == "test":
         adaboostTest(switchFile, modelFile)
-
